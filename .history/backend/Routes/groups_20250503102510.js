f417e0e64c0459eaee26a907a974227095732b0e
adrianjorge.sanchez <adrianjorge.sanchez@students.salle.url.edu>
2025-05-04 01:39:36 +0200

    delete trash

--- .history/backend/Routes/groups_20250503102510.js
+++
@@ -1,9 +0,0 @@
-// Routes/groups.js
-router.post('/:groupId/vote', async (req, res) => {
-    const { destinationId, userId, vote } = req.body;
-    await db.query(
-      'INSERT INTO destination_votes VALUES (?, ?, ?) ON CONFLICT UPDATE vote = ?',
-      [groupId, destinationId, userId, vote]
-    );
-    res.json({ success: true });
-  });
